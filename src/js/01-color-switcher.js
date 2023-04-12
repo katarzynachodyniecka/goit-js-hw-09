6cb539d5fb1a8671b232a058501b2340d30c2207
katarzynachodyniecka <katarzyna.chodyniecka@gmail.com>
2023-04-12 13:45:50 +0200

    # tast 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+let timerID = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
+startBtn.addEventListener('click', () => {
+  startBtn.setAttribute('disabled', true);
+  stopBtn.removeAttribute('disabled');
+  timerID = setInterval(changeColor, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', true);
+  clearInterval(timerID);
+});
