f78af90d0ffe507506428a5cafc72254b6f833b5
katarzynachodyniecka <katarzyna.chodyniecka@gmail.com>
2023-04-14 14:00:15 +0200

    # task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
-function createPromise(position, delay) {
+import Notiflix from 'notiflix';
+
+const delayEl = document.querySelector('input[name="delay"]');
+const stepEl = document.querySelector('input[name="step"]');
+const amountEl = document.querySelector('input[name="amount"]');
+const btnEl = document.querySelector("button");
+
+const createPromise = (position, delay) => {
+  return new Promise ((resolve, reject) => {
+    setTimeout(() => {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    resolve(`Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    reject(` Rejected promise ${position} in ${delay}ms`);
   }
-}
+}, delay);
+});
+}:
+
+const showPromise = () =>{
+  let delay = + delayEl.value;
+
+for (let i = 1; i<=amountEl.value; i++) {
+
+  createPromise(i, delay)
+  .then(success => {
+    Notiflix.Notify.success(success);
+  })
+  .catch(error => {
+    Notiflix.Notify.failure(error);
+  });
+
+  delay += +stepEl.value;
+
+}};
+
+
+
+
+btnEl.addEventListener("click", e => {
+  e.preventDefault();
+  showPromise();
+})
