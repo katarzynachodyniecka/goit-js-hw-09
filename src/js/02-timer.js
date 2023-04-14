a8fe9846c2e1ac29adfa4dc7619af30298ea455a
katarzynachodyniecka <katarzyna.chodyniecka@gmail.com>
2023-04-14 11:38:46 +0200

    # task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,16 +1,79 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
-Notiflix.Notify.success('Sol lucet omnibus');
+const inputEl = document.querySelector('#datetime-picker');
+const btnEl = document.querySelector('[data-start]');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
 
-Notiflix.Notify.failure('Qui timide rogat docet negare');
+btnEl.setAttribute('disabled', '');
 
-// e.g. Message with callback, and the new options
-Notiflix.Notify.success(
-  'Click Me',
-  function cb() {
-    // callback
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    if (options.defaultDate <= selectedDates[0]) {
+      btnEl.removeAttribute('disabled');
+    } else {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      btnEl.setAttribute('disabled', '');
+    }
   },
-  {
-    timeout: 4000,
+};
+flatpickr(inputEl, options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+const addLeadingZero = value => {
+  if (value.toString().length === 1) {
+    return value.toString().padStart(2, '0');
   }
-);
+  return value;
+};
+
+inputEl.addEventListener('input', e => {
+  pickedDate = new Date(e.currentTarget.value).getTime();
+
+  btnEl.addEventListener('click', () => {
+    const countdown = setInterval(() => {
+      const nowDateNumber = new Date().getTime();
+      let timeRemaining = pickedDate - nowDateNumber;
+
+      secondsEl.innerHTML = addLeadingZero(convertMs(timeRemaining).seconds);
+      minutesEl.innerHTML = addLeadingZero(convertMs(timeRemaining).minutes);
+      hoursEl.innerHTML = addLeadingZero(convertMs(timeRemaining).hours);
+      daysEl.innerHTML = addLeadingZero(convertMs(timeRemaining).days);
+
+      if (timeRemaining < 0) {
+        clearInterval(countdown);
+        secondsEl.innerHTML = addLeadingZero(0);
+        minutesEl.innerHTML = addLeadingZero(0);
+        hoursEl.innerHTML = addLeadingZero(0);
+        daysEl.innerHTML = addLeadingZero(0);
+      }
+    }, 1000);
+  });
+});
